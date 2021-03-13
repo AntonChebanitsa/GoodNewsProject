c3088293d75201d0d034cb60ef019a1a4483353b
Anton <antob.chebanitsa@gmail.com>
2021-03-13 15:27:12 +0300

    Some edits

--- GoodNewsProject.Data/Entities/CommentArticle.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GoodNewsProject.Data.Entities
-{
-    public class CommentArticle
-    {
-        public int CommentID { get; set; }
-        public int ArticleID { get; set; }
-    }
-}
--- GoodNewsProject.Data/Entities/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GoodNewsProject.Data.Entities
-{
-    public class Role
-    {
-        public int ID { get; set; }
-        public RoleEnum Name { get; set; }
-    }
-}
